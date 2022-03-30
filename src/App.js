49a1c1d4c24d5b6bff142b6e81ce23b5c19d9272
Valentina <valencondde@gmail.com>
2022-03-30 15:43:42 -0300

    agregue inicio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
-import { Carrusel } from './components/Carrusel';
 import { ItemListContainer } from './components/ItemListContainer';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ItemDetailContainer } from './components/ItemDetailContainer';
 import productos from './components/productos.json'
 import { Inicio } from './components/Inicio';
